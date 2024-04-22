f910601d22e4a504bd0d6037fcad4857d117aae2
Pedro Fontoura <pedrfont@gmail.com>
2024-04-22 19:00:14 -0300

    extremely simple get request and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
   def home
+    storyId = 1024
+    timestamp = Time.now.to_i.to_s 
+    hash = Digest::MD5.hexdigest(timestamp + ENV["PRIVATE_KEY"] + ENV["PUBLIC_KEY"]).to_s
+    response = HTTParty.get("https://gateway.marvel.com:443/v1/public/stories/#{storyId}", query: { apikey: ENV["PUBLIC_KEY"], ts: timestamp, hash: hash})
+    @story = response["data"]["results"].first
+    characters = []
+    @story["characters"]["items"].each do |character|
+      response = HTTParty.get(character["resourceURI"], query: { apikey: ENV["PUBLIC_KEY"], ts: timestamp, hash: hash})
+      thumbnail = response["data"]["results"].first["thumbnail"]
+      character["thumbnail"] = thumbnail["path"] + "." + thumbnail["extension"]
+      characters.push(character)
+    end
+    @characters = characters
+    @attribution = response["attributionHTML"]
   end
 end
