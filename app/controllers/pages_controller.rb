9d3904453957ffd7472ca2aadabeac7751afd249
Pedro Fontoura <pedrfont@gmail.com>
2024-04-23 07:10:58 -0300

    changing logic to pick random stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,27 @@
 class PagesController < ApplicationController
   def home
-    # my chosen story
-    storyId = 1024
-
     # marvel query params
     timestamp = Time.now.to_i.to_s
     privateKey = ENV["PRIVATE_KEY"] || "1234"
     publicKey = ENV["PUBLIC_KEY"] || "1234"
     hash = Digest::MD5.hexdigest(timestamp + privateKey + publicKey).to_s
-    
-    # send request to marvel api
-    response = HTTParty.get("https://gateway.marvel.com:443/v1/public/stories/#{storyId}", query: { apikey: publicKey, ts: timestamp, hash: hash})
-    
+
+    # picking a random story from hawkeye
+    characterId = 1009338
+    storyResponse = HTTParty.get("https://gateway.marvel.com:443/v1/public/stories", query: { characters: characterId, apikey: publicKey, ts: timestamp, hash: hash})
+    @story = storyResponse["data"]["results"].sample
+    storyId = @story["id"]
+
     # error handling
-    if (response["code"] != 200)
+    if (storyResponse["code"] != 200)
       return
     end
 
-    # get story object from response and assign to view
-    @story = response["data"]["results"].first
-
     # new request to fetch characters thumbnails
-    response = HTTParty.get("https://gateway.marvel.com:443/v1/public/stories/#{storyId}/characters", query: { apikey: publicKey, ts: timestamp, hash: hash})
+    charactersResponse = HTTParty.get("https://gateway.marvel.com:443/v1/public/stories/#{storyId}/characters", query: { apikey: publicKey, ts: timestamp, hash: hash})
 
     # assign values to view
-    @characters = response["data"]["results"]
-    @attribution = response["attributionHTML"]
+    @characters = charactersResponse["data"]["results"]
+    @attribution = storyResponse["attributionHTML"]
   end
 end
